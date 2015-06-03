49ba6ff6cd1175debc40eae73164aa5008b86d0e
shohily <shohely.haque@gmail.com>
2015-06-03 09:03:21 -0700

    Admission processing

---
+++ AdmissionProcessiongApp/AdmissionProcessing.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace AdmissionProcessiongApp
+{
+    partial class AdmissionProcessing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hscRollTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sscRollTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.meritOrderTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sscGpaTextBox = new System.Windows.Forms.TextBox();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.displsyButton = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.hscGpaTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Admission ID";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(89, 34);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(196, 20);
+            this.idTextBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "H.S.C Roll";
+            // 
+            // hscRollTextBox
+            // 
+            this.hscRollTextBox.Location = new System.Drawing.Point(89, 78);
+            this.hscRollTextBox.Name = "hscRollTextBox";
+            this.hscRollTextBox.Size = new System.Drawing.Size(196, 20);
+            this.hscRollTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Enter Admission ID";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(170, 390);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(142, 20);
+            this.searchTextBox.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(322, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "S.S.C Roll";
+            // 
+            // sscRollTextBox
+            // 
+            this.sscRollTextBox.Location = new System.Drawing.Point(405, 37);
+            this.sscRollTextBox.Name = "sscRollTextBox";
+            this.sscRollTextBox.Size = new System.Drawing.Size(181, 20);
+            this.sscRollTextBox.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(322, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Merit Order";
+            // 
+            // meritOrderTextBox
+            // 
+            this.meritOrderTextBox.Location = new System.Drawing.Point(405, 121);
+            this.meritOrderTextBox.Name = "meritOrderTextBox";
+            this.meritOrderTextBox.Size = new System.Drawing.Size(181, 20);
+            this.meritOrderTextBox.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(322, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "S.S.C GPA";
+            // 
+            // sscGpaTextBox
+            // 
+            this.sscGpaTextBox.Location = new System.Drawing.Point(405, 81);
+            this.sscGpaTextBox.Name = "sscGpaTextBox";
+            this.sscGpaTextBox.Size = new System.Drawing.Size(181, 20);
+            this.sscGpaTextBox.TabIndex = 4;
+            // 
+            // insertButton
+            // 
+            this.insertButton.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertButton.Location = new System.Drawing.Point(240, 172);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(131, 43);
+            this.insertButton.TabIndex = 6;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // displsyButton
+            // 
+            this.displsyButton.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displsyButton.Location = new System.Drawing.Point(444, 388);
+            this.displsyButton.Name = "displsyButton";
+            this.displsyButton.Size = new System.Drawing.Size(151, 41);
+            this.displsyButton.TabIndex = 8;
+            this.displsyButton.Text = "Display";
+            this.displsyButton.UseVisualStyleBackColor = true;
+            this.displsyButton.Click += new System.EventHandler(this.displsyButton_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView.Location = new System.Drawing.Point(12, 221);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(610, 150);
+            this.dataGridView.TabIndex = 7;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Admission ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "H.S.C Roll Number";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "H.S.C GPA";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "S.S.C Roll Number";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "S.S.C GPA";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Merit Order";
+            this.Column6.Name = "Column6";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(121, 438);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(132, 23);
+            this.searchButton.TabIndex = 10;
+            this.searchButton.Text = "Search Info";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 118);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "H.S.C GPA";
+            // 
+            // hscGpaTextBox
+            // 
+            this.hscGpaTextBox.Location = new System.Drawing.Point(89, 118);
+            this.hscGpaTextBox.Name = "hscGpaTextBox";
+            this.hscGpaTextBox.Size = new System.Drawing.Size(196, 20);
+            this.hscGpaTextBox.TabIndex = 2;
+            // 
+            // AdmissionProcessing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 493);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.displsyButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.sscGpaTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.meritOrderTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sscRollTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.hscGpaTextBox);
+            this.Controls.Add(this.hscRollTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "AdmissionProcessing";
+            this.Text = "Admission Processing System";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox hscRollTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox sscRollTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox meritOrderTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox sscGpaTextBox;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button displsyButton;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox hscGpaTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ AdmissionProcessiongApp/AdmissionProcessing.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdmissionProcessiongApp
+{
+    public partial class AdmissionProcessing : Form
+    {
+        public AdmissionProcessing()
+        {
+            InitializeComponent();
+        }
+
+         string connectionstring = @"server =USER;database=DB_AdmissionProcessingSystem;integrated security=true";
+         int admissionID;
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+             admissionID = Convert.ToInt16(idTextBox.Text);
+            int hscRoll = Convert.ToInt16(hscRollTextBox.Text);
+            double hscGpa = Convert.ToDouble(hscGpaTextBox.Text);
+            int sscRoll = Convert.ToInt16(sscRollTextBox.Text);
+            double sscGpa = Convert.ToDouble(sscGpaTextBox.Text);
+            int meritOrder = Convert.ToInt16(meritOrderTextBox.Text);
+
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+
+            string query =
+                string.Format(@"insert into Table_AdmissionProcessing(AdmissionID,HscRollNo,HscGpa,SscRollNo,SscGpa,MeritOrder)
+                                 values('{0}','{1}','{2}','{3}','{4}','{5}')", admissionID, hscRoll, hscGpa, sscRoll,
+                    sscGpa, meritOrder);
+            SqlCommand aCommand = new SqlCommand(query, aConnection);
+            MessageBox.Show("Data has been inserted");
+            idTextBox.Text = "";
+            hscRollTextBox.Text = "";
+            hscGpaTextBox.Text = "";
+            sscRollTextBox.Text = "";
+            sscGpaTextBox.Text = "";
+            meritOrderTextBox.Text = "";
+            aCommand.ExecuteNonQuery();
+            aConnection.Close();
+        }
+
+        private void displsyButton_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            dataGridView.Refresh();
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+            string tableSql = "SELECT * FROM Table_AdmissionProcessing";
+            SqlCommand selectAllTableSqlCommand = new SqlCommand(tableSql, aConnection);
+            SqlDataReader tableReader = selectAllTableSqlCommand.ExecuteReader();
+
+            if (tableReader != null)
+                while (tableReader.Read())
+                {
+                    dataGridView.Rows.Add(tableReader[0].ToString(), tableReader[1].ToString(),
+                        tableReader[2].ToString(), tableReader[3].ToString(),
+                        tableReader[4].ToString(), tableReader[5].ToString());
+                }
+            tableReader.Close();
+            aConnection.Close();
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            dataGridView.Refresh();
+           
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+            admissionID = Convert.ToInt16(searchTextBox.Text);
+
+
+            SqlCommand searchByAdmissionID =
+                new SqlCommand("SELECT * FROM Table_AdmissionProcessing WHERE AdmissionID like '" + admissionID + "'", aConnection);
+
+            SqlDataReader tableReader = searchByAdmissionID.ExecuteReader();
+            if (tableReader != null)
+            {
+                bool check = true;
+                while (tableReader.Read())
+                {
+                    dataGridView.Rows.Add(tableReader[0].ToString(), tableReader[1].ToString(),
+                        tableReader[2].ToString(), tableReader[3].ToString(),
+                        tableReader[4].ToString(), tableReader[5].ToString());
+                    check = false;
+                }
+                if (check)
+                {
+                    MessageBox.Show("Your desired admission ID information is not present in the database. ");
+                }
+                tableReader.Close();
+                aConnection.Close();
+            }
+        }
+    }
+}
